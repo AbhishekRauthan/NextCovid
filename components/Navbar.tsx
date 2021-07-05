e62fdea27502f8928fa73be040fde46332f0da3d
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2021-07-05 22:04:50 +0530

    made navbar customized for larger screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,22 @@ const Navbar: FC = () => {
 
   return (
     <header className="bg-gray-50 ">
-      <div className="container mx-auto flex w-full justify-between items-start xs:px-3 xs:py-3 shadow-2xl">
-        <h1 className="xs:text-xl sm:text-3xl capitalize font-bold font-source">
-          Covid 19 tracker
-        </h1>
-        <span className="cursor-pointer" onClick={() => setIsClosed(!isClosed)}>
+      <div className="container flex md:flex-row justify-between items-center xs:p-3 sm:px-4 shadow-2xl xs:relative">
+        <div className="block">
+          <h1 className="xs:text-xl sm:text-4xl sm:py-3 capitalize font-bold font-source">
+            Covid 19 tracker
+          </h1>
+        </div>
+        <div className="absolute xs:top-12 xs:right-1 sm:relative xs:flex xs:flex-col xs:items-end  xs:shadow-2xl">
+          <nav
+            className={`${isClosed ? "xs:hidden" : ""
+              } flex xs:flex-col flex-row justify-evenly w-max xs:p-3 bg-gray-50`}
+          >
+            <li className="nav-item">about this tracker</li>
+            <li className="nav-item">measures to take</li>
+          </nav>
+        </div>
+        <span className="cursor-pointer sm:hidden" onClick={() => setIsClosed(!isClosed)}>
           <svg
             className={isClosed ? `xs:h-6 xs:w-6 sm:hidden` : "hidden"}
             xmlns="http://www.w3.org/2000/svg"
@@ -39,16 +50,7 @@ const Navbar: FC = () => {
             />
           </svg>
         </span>
-      </div>
-      <div className="absolute right-0 sm:relative flex flex-col items-end  xs:shadow-2xl">
-        <nav
-          className={`${
-            isClosed ? "xs:hidden" : ""
-          } flex xs:flex-col flex-row justify-evenly w-max xs:p-3 bg-gray-50`}
-        >
-          <li className="nav-item">about this tracker</li>
-          <li className="nav-item">measures to take</li>
-        </nav>
+
       </div>
     </header>
   );
