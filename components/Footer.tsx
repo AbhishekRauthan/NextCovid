615ae27a35e4ef10a059161ed25e98177838ad53
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2021-07-13 17:02:34 +0530

    done with footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,14 @@ import { FC } from "react";
 const Footer: FC = () => {
   return (
     <footer className="bg-gray-700">
-      <div className="container mx-auto grid xs:grid-cols-1 xs:py-10 text-gray-50   xs:text-center">
-        <h1 className="xs:text-xl font-source font-bold">Covid 19 Tracker</h1>
-        <ul className="flex xs:flex-col xs:my-4 xs:align-middle">
-          <li className="footer-link">Map</li>
-          <li className="footer-link xs:mt-3">About this tracker</li>
-          <li className="footer-link xs:mt-3">Measures to Take</li>
+      <div className="container mx-auto py-10 sm:py-10 text-gray-50 text-center">
+        <h1 className="text-xl sm:text-3xl md:text-4xl font-source font-bold">Covid 19 Tracker</h1>
+        <ul className="flex flex-col md:flex-row items-center md:items-baseline md:justify-evenly text-center my-4">
+          <li className="footer-link md:ml-10">goto Map</li>
+          <li className="footer-link mt-3 md:m-0">About this tracker</li>
+          <li className="footer-link mt-3 md:m-0">Measures to Take</li>
         </ul>
-        <p className="xs:text-sm xs:mt-2 text-gray-300">this site is for educational purpose only <br/> Created by Abhishek</p>
+        <p className="text-sm md:text-lg lg:text-xl mt-2 md:pt-5 text-gray-200">this site is for educational purpose only <br/> Created by Abhishek</p>
       </div>
     </footer>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <div className="xs:w-72 w-10/12 md:w-11/12 lg:h-full lg:w-9/12 mx-auto my-10">
       <div className="shadow-2xl text-center bg-white flex xs:p-5  xs:flex-col-reverse lg:h-2/5 xs:my-5 mx-auto">
         <div className="flex flex-col sm:my-10 sm:w-1/2 md:my-auto md:pb-9">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl capitalize  font-source text-gray-900 xs:pt-4">
+          <h1 className="text-3xl sm:pt-8 md:text-4xl lg:text-5xl capitalize  font-source text-gray-900 xs:pt-4">
             Covid 19 tracker
           </h1>
           <p className="xs:text-md md:text-lg lg:text-xl text-gray-700 xs:pt-2 sm:mt-3 font-roboto">
