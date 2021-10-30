0f10c10c808b7fb39fd266a9090e7d9b1cd5506b
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-10-30 05:13:07 +0000

    The following changes were done:
      - dark background body color removed from main css file
      - center container as default in tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}'
-  ],
-  mode: 'jit',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      screens : {
-        'xs': {'max': '639px'}
+      screens: {
+        xs: { max: "639px" },
       },
       fontFamily: {
-        'source':['Source Sans Pro', 'sans-serif'],
-        'roboto':['Roboto', 'sans-serif'],
-      }
+        source: ["Source Sans Pro", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      container: {
+        center: true,
+      },
     },
   },
   variants: {
     extend: {
-      transform: ['hover', 'focus'],
+      transform: ["hover", "focus"],
     },
   },
   plugins: [],
-}
+};
