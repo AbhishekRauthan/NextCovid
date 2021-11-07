6364d4bca57265eb133f99b31d48146d3253b3e3
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-11-07 22:30:17 +0530

    The following changes were done:   - simplified about page by using one button only   - removed unused imports

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,8 @@
-import Layout from "@components/Layout"
-import AboutSection from "@components/AboutSections"
-import Button from "@components/Button"
-import Head from 'next/head'
-import AboutContainer from '@components/AboutContainer'
+import Layout from "@components/Layout";
+import AboutSection from "@components/AboutSections";
+import Button from "@components/Button";
+import Head from "next/head";
+import AboutContainer from "@components/AboutContainer";
 
 const About = () => {
   return (
@@ -13,42 +13,52 @@ const About = () => {
       <Layout>
         <AboutContainer title="About">
           <AboutSection title="Is this official?">
-            No. This is an open source project created to spread information about Covid-19.
+            No. This is an open source project created to spread information
+            about Covid-19.
           </AboutSection>
 
           <AboutSection title="purpose of this tracker">
-            Main purpose of this tracker is to provide users an upto date knowlegde of Covid-19 and related information like recovery rate, spread, about vaccines etc
+            Main purpose of this tracker is to provide users an upto date
+            knowlegde of Covid-19 and related information like recovery rate,
+            spread, about vaccines etc
           </AboutSection>
 
-
           <AboutSection title="about covid-19">
-            COVID-19 is the disease caused by a new coronavirus called SARS-CoV-2.  WHO first learned of this new virus on 31 December 2019, following a report of a cluster of cases of ‘viral pneumonia’ in Wuhan, People’s Republic of China.
-            The disease has since spread worldwide, leading to an ongoing pandemic.
+            COVID-19 is the disease caused by a new coronavirus called
+            SARS-CoV-2. WHO first learned of this new virus on 31 December 2019,
+            following a report of a cluster of cases of ‘viral pneumonia’ in
+            Wuhan, People’s Republic of China. The disease has since spread
+            worldwide, leading to an ongoing pandemic.
             <br />
-            <div className="mt-2">
-              <Button secondary content="Click to learn More" link="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19" />
-            </div>
           </AboutSection>
 
           <AboutSection title="symtoms of covid">
-            Symtoms of covid are variable, but often include fever, cough, headache, fatigue, breathing difficulties, and loss of smell and taste. Symptoms may begin one to fourteen days after exposure to the virus.
+            Symtoms of covid are variable, but often include fever, cough,
+            headache, fatigue, breathing difficulties, and loss of smell and
+            taste. Symptoms may begin one to fourteen days after exposure to the
+            virus.
             <br />
-            <div className="mt-2">
-              <Button secondary content="Click to learn More" link="/meassures" />
-            </div>
           </AboutSection>
 
           <AboutSection title="Protection against covid">
-            Stay safe by taking some simple precautions, such as physical distancing, wearing a mask, especially when distancing cannot be maintained, keeping rooms well ventilated, avoiding crowds and close contact, regularly cleaning your hands, and coughing into a bent elbow or tissue. Check local advice where you live and work.
+            Stay safe by taking some simple precautions, such as physical
+            distancing, wearing a mask, especially when distancing cannot be
+            maintained, keeping rooms well ventilated, avoiding crowds and close
+            contact, regularly cleaning your hands, and coughing into a bent
+            elbow or tissue. Check local advice where you live and work.
             <br />
-            <div className="mt-2">
-              <Button secondary content="Click to learn More" link="/meassures" />
-            </div>
           </AboutSection>
+          <div className="mt-2 mx-auto">
+            <Button variant="secondary">
+              <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19">
+                Learn more in WHO official site
+              </a>
+            </Button>
+          </div>
         </AboutContainer>
-      </Layout >
+      </Layout>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
