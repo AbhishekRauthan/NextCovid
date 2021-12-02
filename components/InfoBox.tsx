302604085b0bf6b1d90d0844a1c0b08bce0c208c
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-12-02 20:43:02 +0530

    the following changes were done:
      - added total prop for all time total stat
      - added rest of div props
      - added padding and text center
      - display today cases and total

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -4,18 +4,21 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   active: boolean;
   title: string;
   cases: string;
+  total: string;
 }
 
-const InfoBox: FC<Props> = ({ active, cases, onClick, title }) => {
+const InfoBox: FC<Props> = ({ active, cases, onClick, title, total, ...rest }) => {
   return (
     <div
-      className={`shadow bg-gray-100 border-2 border-transparent cursor-pointer ${
-        active && " border-gray-600 shadow-lg"
+      className={`text-center shadow bg-gray-100 border-2 p-2.5 border-transparent cursor-pointer ${
+        active && "border-gray-600 shadow-lg"
       }`}
       onClick={onClick}
+      {...rest}
     >
       <h1 className="text-2xl">{title}</h1>
-      <p>{cases}</p>
+      <p>{cases} today</p>
+      <p>{total} total</p>
     </div>
   );
 };
