3968c33872a3a18cf1e2798eca189112fbcc923a
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-12-02 20:53:58 +0530

    The following changes were done:
      - added total function for formatting
      - added conditional formatting to prettyPrintStat

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,6 @@
 import numeral from "numeral";
 
 export const prettyPrintStat = (stat: number) =>
-  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+  stat > 1000 ? `+${numeral(stat).format("0.0a")}` : `${stat}`;
+
+export const total = (total: number) => numeral(total).format("0.0a");
