e276c564d7fad3be37c291884ca0dac756f9cc34
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-12-03 23:01:29 +0530

    The following changes were done:
      - added a react leaflet map
      - provided it with all necessary props and style
      - added a function to change center of map according to given location

---
+++ components/LeafletMap.tsx
@@ -0,0 +1,91 @@
+import { CountryData } from "@interface/MapData";
+import { CasesType } from "@interface/PropType";
+import numeral from "numeral";
+import { Circle, MapContainer, Popup, TileLayer, useMap } from "react-leaflet";
+
+type Props = {
+  countries: CountryData[];
+  casesType: CasesType;
+  center: {
+    lat: number;
+    lng: number;
+  };
+};
+
+const casesTypeColors = {
+  cases: {
+    hex: "#DC2626",
+    multiplier: 200,
+  },
+  recovered: {
+    hex: "#16A34A",
+    multiplier: 300,
+  },
+  deaths: {
+    hex: "#475569",
+    multiplier: 800,
+  },
+};
+
+function SetViewOnClick({ coords }) {
+  const map = useMap();
+  map.setView(coords, map.getZoom());
+
+  return null;
+}
+
+const LeafletMap = ({ casesType, countries, center }: Props) => {
+  return (
+    <div className="h-[500px] bg-white shadow-lg p-4 mt-4 w-full">
+      <MapContainer zoom={3} center={center} className="h-full">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <SetViewOnClick coords={center} />
+        {countries.map((country) => (
+          <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            pathOptions={{
+              color: casesTypeColors[casesType].hex,
+              fillColor: casesTypeColors[casesType].hex,
+            }}
+            fillOpacity={0.4}
+            radius={
+              Math.sqrt(country[casesType]) *
+              casesTypeColors[casesType].multiplier
+            }
+            key={country.country}
+          >
+            <Popup>
+              <div className="w-36">
+                <div
+                  className="w-24 rounded"
+                  style={{
+                    backgroundImage: `url(${country.countryInfo.flag})`,
+                    width: "100px",
+                    borderRadius: "5px",
+                  }}
+                ></div>
+                <div className="text-base font-bold text-gray-800">
+                  {country.country}
+                </div>
+                <div className="">
+                  Cases: {numeral(country.cases).format("0,0")}
+                </div>
+                <div className="">
+                  Recovered: {numeral(country.recovered).format("0,0")}
+                </div>
+                <div className="">
+                  Deaths: {numeral(country.deaths).format("0,0")}
+                </div>
+              </div>
+            </Popup>
+          </Circle>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default LeafletMap;
