d668ed0ce82264b303ee9b9e63c9dbc9fb1a9566
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-04-10 17:25:40 +0530

    The following changes were done:
    	- updated tailwind config file for v3 support
    	- removed animations and keyframes from tailwind config
    	- converted main.css to main.scss
    	- added required animations and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  mode: "jit",
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
@@ -14,39 +12,7 @@ module.exports = {
       container: {
         center: true,
       },
-      animation: {
-        load: "up 1s cubic-bezier(0.23, 0.2, 1, 1.22)",
-      },
-      keyframes: {
-        up: {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(1.7rem)",
-          },
-          "100%": {
-            opacity: "1",
-            transform: "translateY(0rem)",
-          },
-        },
-      },
-    },
-  },
-  variants: {
-    extend: {
-      transform: ["hover", "focus"],
     },
   },
   plugins: [],
 };
-
-// @keyframes load {
-//   from {
-//     display: none;
-//     transform: translateY(-30);
-//   }
-
-//   to {
-//     display: block;
-//     transform: translateY(0);
-//   }
-// }
