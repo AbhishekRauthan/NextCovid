60e3055c90bb8d8f9d1196c46f50b50e908aacd7
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-04-10 17:41:10 +0530

    The following changes were done:
    	- added an order prop for using a order css variable
    	- using order prop, added animation delays according to order
    	- reference: https://youtu.be/Qhaz36TZG5Y?t=407

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,17 @@
-import { FC } from "react";
+import { CSSProperties, FC } from "react";
 
 interface Props {
   text: string;
   imageSrc: string;
   imageAlt: string;
+  order: CSSProperties;
 }
 
-const Card: FC<Props> = ({ imageSrc, imageAlt, text }) => {
+const Card: FC<Props> = ({ imageSrc, imageAlt, text, order }) => {
   return (
     <div
-      className={`bg-gray-50 w-3/4 md:mx-3.5 shadow-2xl border-b-4 border-gray-800 mx-auto`}
+      className={`bg-gray-50 w-3/4 md:mx-3.5 shadow-2xl border-b-4 border-gray-800 mx-auto up-animation`}
+      style={order}
     >
       <img
         className="w-3/6 h-3/4 lg:h-1/2 sm:mb-5 mx-auto xs:pt-2 xs:mt-2"
